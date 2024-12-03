fb59f63127b923b05fe5dcaa15a1915de32ec047
Хмура Андрей <khmurahottest@gmail.com>
2024-12-03 21:12:11 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-time=int(input("Введите часы:"))
-time1=int(input("Введите минуты:"))
-raz=int(input("Введите разницу во времени "))
-p=time+raz
-if p>=24:
-    p-=24
-if p<0:
-    p+=24
-print("Полученное время:",p,":",time1)
