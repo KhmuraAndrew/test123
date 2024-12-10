f3ad3316391828f8f15e34974ce1e2ed44813e14
Хмура Андрей <khmurahottest@gmail.com>
2024-12-10 21:10:51 +0300

    Add files via upload

--- рое.py
+++ рое.py
@@ -1,5 +1,6 @@
 import string
 import random
+k=int(input("Введите колличество паролей:"))
 d=int(input("Введите длинну пароля:"))
 s=int(input("Введите сложность пароля [1-3]:"))
 a=""
@@ -10,6 +11,8 @@ elif s==2:
 elif s==3:
     a=string.ascii_letters+string.digits+string.punctuation
 r=""
-for i in range(d):
-    r+= random.choice(a)
-print(f'Ваш пароль:{r}')
+for _ in range(k):
+    r = ""
+    for i in range(d):
+        r+= random.choice(a)
+    print("Ваш пароль",r)
